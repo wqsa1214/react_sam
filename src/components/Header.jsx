c6bffa9e49276d1776dcd5b2b6bc2cd4a03ac47e
wqsa1214 <wqsa1214@naver.com>
2024-07-29 12:11:23 +0900

    react_setting완성

---
+++ src/components/Header.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react"
+
+  const headerNav =[
+    {
+      title:"intro",
+      url:"#intro",
+    },
+    {
+      title:"skill",
+      url:"#skill",
+    },
+    {
+      title:"site",
+      url:"#site",
+    },
+    {
+      title:"port",
+      url:"#port",
+    },
+    {
+      title:"contact",
+      url:"#contact",
+    },
+    
+  ]
+
+const Header = () => {
+  
+  // const [show,setShow] = useState(false);
+  const [show,setShow]=useState(false)
+  const toggleMenu = () => {
+    setShow((prevShow) => ! prevShow);
+  }
+  // const show = "";
+
+  return (
+    <header id="header" role="banner">
+      <div className="header_inner">
+        <div className="header_logo">
+          <a href="/">portfolio <em>react</em></a>
+        </div>
+        <div 
+        className={`header_nav ${show ? "show":""}`} 
+        role="navigation" 
+        aria-label="메인 메뉴">
+
+          <ul>
+            {headerNav.map((nav,key)=>(
+                <li key={key}>
+                  <a href="{nav.url}">{nav.title}</a>
+                </li>
+            ))}
+
+
+
+
+
+
+          </ul>
+        </div>
+        <div 
+            className="header_nav_moblie"
+            id="headerToggle"
+            aria-controls="primary_menu"
+            aria-expanded={show ? "true":"false"}
+            role="button"
+            tabIndex="0"
+            onClick={toggleMenu}
+        >
+            <span></span>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
